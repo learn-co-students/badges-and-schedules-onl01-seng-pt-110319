22817c003168d317993e48c68a0ecd0ae9b41052
Nory Lazo <nlazo@icloud.com>
2019-11-08 06:17:22 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,5 +20,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
+  batch_badge_creator(attendees).each do |badges|
+    puts badges
+  end
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
 end 
