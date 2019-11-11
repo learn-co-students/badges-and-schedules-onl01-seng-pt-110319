c216aee04b30c9aa5ab5a30471bd8993f064a221
Jordan Hayes <nosafoundation@gmail.com>
2019-11-11 23:08:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+conference_badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"] 
+  
+
+badge_maker(conference_badges)
+class name 
+  include Enumerable
+def each 
+  yield 1 
+end
+batch_badge_maker
+
+assign_rooms
+
+printer 
